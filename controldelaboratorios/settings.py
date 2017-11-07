2e0b4846991745cc8291d837fa7d5d360cc8ddf8
luma1103 <lumamor1103@gmail.com>
2017-11-07 12:38:02 -0600

    holi

--- controldelaboratorios/settings.py
+++ controldelaboratorios/settings.py
@@ -122,6 +122,7 @@ LOGIN_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
